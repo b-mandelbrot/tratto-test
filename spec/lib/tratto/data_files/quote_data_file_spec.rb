9b1e06ff31c9b1c162208f96c7a04e50b7739709
Bruno Barros <brunobarros@id.uff.br>
2018-02-14 07:43:53 -0200

    Add wallet central

--- spec/lib/tratto/data_files/quote_data_file_spec.rb
+++ spec/lib/tratto/data_files/quote_data_file_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe QuoteDataFile do
     subject { QuoteDataFile.new.data }
 
     it { expect(subject).to be_an Array }
-    it { expect(subject.size).to be 3 }
+    it { expect(subject.size).to eq 3 }
     it { expect(subject).to all(be_an(Quote)) }
   end
 end
--- spec/lib/tratto/data_files/wallet_data_file_spec.rb
+++ spec/lib/tratto/data_files/wallet_data_file_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe WalletDataFile do
     subject { WalletDataFile.new.data }
 
     it { expect(subject).to be_an Array }
-    it { expect(subject.size).to be 20 }
+    it { expect(subject.size).to eq 20 }
     it { expect(subject).to all(be_an(Wallet)) }
   end
 end
